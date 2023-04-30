9d73ee82d73e879025863ae2e1de114983dd75e7
NotACat1 <notacat.mail@gmail.com>
2023-04-30 19:30:36 +0400

    Основные страницы и блоки

---
+++ blocks/page/page.js
@@ -0,0 +1,81 @@
+// Фрезерные станки
+// Deprag W52
+let Info__DepragW52 = document.querySelector('#Info__Deprag-W52');
+let btnOpen__DepragW52 = document.querySelector('#btn-open__Deprag-W52');
+let btnClose__DepragW52 = document.querySelector('#btn-close__Deprag-W52');
+
+btnOpen__DepragW52.onclick = function() {
+  Info__DepragW52.style.display = "flex";
+}
+
+btnClose__DepragW52.onclick = function() {
+  Info__DepragW52.style.display = "";
+}
+
+// Deprag W42
+let Info__DepragW42 = document.querySelector('#Info__Deprag-W42');
+let btnOpen__DepragW42 = document.querySelector('#btn-open__Deprag-W42');
+let btnClose__DepragW42 = document.querySelector('#btn-close__Deprag-W42');
+
+btnOpen__DepragW42.onclick = function() {
+  Info__DepragW42.style.display = "flex";
+}
+
+btnClose__DepragW42.onclick = function() {
+  Info__DepragW42.style.display = "";
+}
+
+// Deprag W40
+let Info__DepragW40 = document.querySelector('#Info__Deprag-W40');
+let btnOpen__DepragW40 = document.querySelector('#btn-open__Deprag-W40');
+let btnClose__DepragW40 = document.querySelector('#btn-close__Deprag-W40');
+
+btnOpen__DepragW40.onclick = function() {
+  Info__DepragW40.style.display = "flex";
+}
+
+btnClose__DepragW40.onclick = function() {
+  Info__DepragW40.style.display = "";
+}
+
+//==========================================================================
+
+// Другие товары
+// Deprag P7
+let Info__DepragP7 = document.querySelector('#Info__Deprag-P7');
+let btnOpen__DepragP7 = document.querySelector('#btn-open__Deprag-P7');
+let btnClose__DepragP7 = document.querySelector('#btn-close__Deprag-P7');
+
+btnOpen__DepragP7.onclick = function() {
+  Info__DepragP7.style.display = "flex";
+}
+
+btnClose__DepragP7.onclick = function() {
+  Info__DepragP7.style.display = "";
+}
+
+// Deprag S7
+let Info__DepragS7 = document.querySelector('#Info__Deprag-S7');
+let btnOpen__DepragS7 = document.querySelector('#btn-open__Deprag-S7');
+let btnClose__DepragS7 = document.querySelector('#btn-close__Deprag-S7');
+
+btnOpen__DepragS7.onclick = function() {
+  Info__DepragS7.style.display = "flex";
+}
+
+btnClose__DepragS7.onclick = function() {
+  Info__DepragS7.style.display = "";
+}
+
+// Deprag S7
+let Info__Aoralscan3 = document.querySelector('#Info__Aoralscan3');
+let btnOpen__Aoralscan3 = document.querySelector('#btn-open__Aoralscan3');
+let btnClose__Aoralscan3 = document.querySelector('#btn-close__Aoralscan3');
+
+btnOpen__Aoralscan3.onclick = function() {
+  Info__Aoralscan3.style.display = "flex";
+}
+
+btnClose__Aoralscan3.onclick = function() {
+  Info__Aoralscan3.style.display = "";
+}
