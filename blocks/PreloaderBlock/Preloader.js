b397c2a02c5559c9144c8b980142a34a8a4152f0
NotACat1 <notacat.mail@gmail.com>
2023-05-02 15:15:25 +0400

    Исправление прелодера

--- blocks/PreloaderBlock/Preloader.js
+++ blocks/PreloaderBlock/Preloader.js
@@ -12,6 +12,6 @@ let t2 = gsap.timeline()
 window.onload = function() {
   tl.pause();
   t2.to(".PreloaderBlock__text_transparent", { duration: 1, width: "100%" });
-  t2.to(".PreloaderBlock", { duration: 1.2, y: '-100vh' }, 1);
+  t2.to(".PreloaderBlock", { duration: 1.2, y: '-130vh' }, 1);
   t2.to(".PreloaderBlock", { duration: 0, display: "none" }, 2.2);
 };
