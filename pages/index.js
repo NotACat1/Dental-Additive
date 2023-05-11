87c17bf854d3f20dd2d47a25d9ada125f9ff336a
NotACat1 <notacat.mail@gmail.com>
2023-05-11 21:06:46 +0400

    Исправление изображения и кнопок

--- pages/index.js
+++ pages/index.js
@@ -120,6 +120,60 @@ btnClose__AutoScanDSEXPro_H.onclick = function() {
   Info__AutoScanDSEXPro_H.style.display = "";
 }
 
+//==========================================================================
+
+// Deprag NOVA Esthetics
+let Info__DepragNOVAEsthetics = document.querySelector('#Info__DepragNOVAEsthetics');
+let btnOpen__DepragNOVAEsthetics = document.querySelector('#btn-open__DepragNOVAEsthetics');
+let btnClose__DepragNOVAEsthetics = document.querySelector('#btn-close__DepragNOVAEsthetics');
+
+btnOpen__DepragNOVAEsthetics.onclick = function() {
+  Info__DepragNOVAEsthetics.style.display = "flex";
+}
+
+btnClose__DepragNOVAEsthetics.onclick = function() {
+  Info__DepragNOVAEsthetics.style.display = "";
+}
+
+// Deprag NOVA Amber
+let Info__DepragNOVAAmber = document.querySelector('#Info__DepragNOVAAmber');
+let btnOpen__DepragNOVAAmber = document.querySelector('#btn-open__DepragNOVAAmber');
+let btnClose__DepragNOVAAmber = document.querySelector('#btn-close__DepragNOVAAmber');
+
+btnOpen__DepragNOVAAmber.onclick = function() {
+  Info__DepragNOVAAmber.style.display = "flex";
+}
+
+btnClose__DepragNOVAAmber.onclick = function() {
+  Info__DepragNOVAAmber.style.display = "";
+}
+
+// Deprag Gemstone SHT-Multi
+let Info__DepragGemstoneSHTMulti = document.querySelector('#Info__DepragGemstoneSHTMulti');
+let btnOpen__DepragGemstoneSHTMulti = document.querySelector('#btn-open__DepragGemstoneSHTMulti');
+let btnClose__DepragGemstoneSHTMulti = document.querySelector('#btn-close__DepragGemstoneSHTMulti');
+
+btnOpen__DepragGemstoneSHTMulti.onclick = function() {
+  Info__DepragGemstoneSHTMulti.style.display = "flex";
+}
+
+btnClose__DepragGemstoneSHTMulti.onclick = function() {
+  Info__DepragGemstoneSHTMulti.style.display = "";
+}
+
+// Deprag Gemstone 3D Pro-Multi
+let Info__DepragGemstone3DProMulti = document.querySelector('#Info__DepragGemstone3DProMulti');
+let btnOpen__DepragGemstone3DProMulti = document.querySelector('#btn-open__DepragGemstone3DProMulti');
+let btnClose__DepragGemstone3DProMulti = document.querySelector('#btn-close__DepragGemstone3DProMulti');
+
+btnOpen__DepragGemstone3DProMulti.onclick = function() {
+  Info__DepragGemstone3DProMulti.style.display = "flex";
+}
+
+btnClose__DepragGemstone3DProMulti.onclick = function() {
+  Info__DepragGemstone3DProMulti.style.display = "";
+}
+
 // Super Vision
 let Info__SuperVision = document.querySelector('#Info__SuperVision');
 let btnOpen__SuperVision = document.querySelector('#btn-open__SuperVision');
